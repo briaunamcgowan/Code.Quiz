905af7b6836993255a1d992e6cb0662cf5474dd9
briaunamcgowan <briauna.mcgowan@gmail.com>
2020-11-26 09:48:52 -0600

    pushing question in js

---
+++ script.js
@@ -0,0 +1,27 @@
+var questions = [
+    {Question: "An array's length can be evaluate with what property?",
+    Options: [".loop", ".log", ".length", ".distance"],
+    Answer: ".length"
+    },
+
+    {Question: "The condition in an if/else statment is enclosed within _____.",
+    Options: ["curly brackets", "semi colons", "parentheses", "quotation marks" ],
+    Answer: "parentheses"
+    },
+
+    {Question: "The logical operator that represents 'and' is ____.",
+    Options: ["AND", "&&", "||", "="],
+    Answer: "&&"
+    },
+
+    {Question: "What does DOM stand for?",
+    Options: ["Desktop Oriented Mode", "Document Object Model", "Digital Oridance Model", "Display Object Management"],
+    Answer: "Document Object Model"
+    },
+
+    {Question: "Is JavaScript case-sensitive?",
+    Options: ["yes", "no", "maybe", "sometimes"],
+    Answer: ["yes"]
+},
+    
+];
